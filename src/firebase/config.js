0986d1d9430dc1cd035eecaf0889bcea9e1d97e4
rajesh6161 <rajeshranjan2712000@gmail.com>
2020-10-19 20:01:09 +0530

    initilized project

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAxeTgRQ1ai9FW78zPtXjvsSasrQazCO3M",
+    authDomain: "firegram-b4bfe.firebaseapp.com",
+    databaseURL: "https://firegram-b4bfe.firebaseio.com",
+    projectId: "firegram-b4bfe",
+    storageBucket: "firegram-b4bfe.appspot.com",
+    messagingSenderId: "330860934951",
+    appId: "1:330860934951:web:cff82a69162935960a9b02"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
